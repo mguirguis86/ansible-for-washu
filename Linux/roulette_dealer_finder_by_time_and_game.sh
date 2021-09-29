a0a89fa8eddb08050014dda38bc8f8d41e7c6944
mguirguis86 <90808951+mguirguis86@users.noreply.github.com>
2021-09-29 18:39:08 -0400

    Add files via upload

---
+++ Linux/roulette_dealer_finder_by_time_and_game.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Input a date (MMDD) and time (00:00:00 AM/PM)
+
+read -p 'Date: ' DATE
+read -p 'Time: ' TIME
+
+FILE="${DATE}_Dealer_schedule"
+
+grep -i "$TIME" "$FILE" | awk '{ print $1,$2,$3,$4 }'
